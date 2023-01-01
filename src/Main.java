9bf612b814f4fae84c5dae4009cdbf04294b75e7
unik0 <murotkafbi@gmail.com>
2023-01-01 20:40:18 +0300

    HomeWork3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задаиние 1
+        System.out.println("Задаине 1");
+        int age = 18;
+        if (age >18 && age <=9) {
+            System.out.println(age +"Он совершеннолетний ");
+        }
+        // Задаиие 2
+
+    }
+}
